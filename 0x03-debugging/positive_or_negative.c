be50ca80ffca2908939798ed67cc48b3bcbae9a6
michelleikeh <michelle.ikeh@gmail.com>
2022-10-20 12:15:19 -0700

    add task 0-positive_or_negative.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - checks if param is positive or negative
+ *
+ * @i: number to check
+ *
+ * Return: void
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	else
+	{
+		printf("%d is zero\n", i);
+	}
+}			

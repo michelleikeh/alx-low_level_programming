d3209bc7771e5a0620bcafe88cccb032eeebebd1
michelleikeh <michelle.ikeh@gmail.com>
2022-10-21 12:23:40 -0700

    fix fizz buzz spacing

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,15 +14,15 @@ int main(void)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			printf("FIZZBUZZ");
+			printf(" FizzBuzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("FIZZ");
+			printf(" Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("BUZZ");
+			printf(" Buzz");
 		}
 		else
 		{

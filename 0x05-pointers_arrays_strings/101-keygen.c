ede1d274551753297c015c1bd39599e8adb4b73f
michelleikeh <michelle.ikeh@gmail.com>
2022-10-24 21:31:43 -0700

    Advance task 101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.b>
+#include <time.h>
+
+/**
+ * main- keygen
+ * Return:Always Success
+ */
+
+int main(void)
+{
+	int r = 0, i = 0;
+	time_t t;
+
+	srand(unsigned int) time(&t));
+	while (i < 2772)
+	{
+		r = rand() % 128;
+		if ((i + r) > 2772)
+			break;
+		i = i + r;
+		printf("%c", r);
+	}
+	printf("%c\n", (2772 - i));
+	return (0);
+}
